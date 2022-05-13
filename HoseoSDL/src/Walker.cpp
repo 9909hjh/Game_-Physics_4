ca08f1805c141fef0648e72a1f1880f690ca86d1
9909hjh <9909hjh@gmail.com>
2022-05-13 14:19:39 +0900

    random 문제 해결
    random 문제 해결

--- HoseoSDL/src/Walker.cpp
+++ HoseoSDL/src/Walker.cpp
@@ -1,16 +1,16 @@
 #include "Walker.h"
-//#include <random>
+#include <random>
 #define WIDTH (600)
 #define HEIGHT (400)
 
-//// 시드값을 얻기 위한 random_device 생성.
-//std::random_device rd;
-//
-//// random_device 를 통해 난수 생성 엔진을 초기화 한다.
-//std::mt19937 gen(rd());
-//
-//// 0 부터 3까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의.
-//std::uniform_int_distribution<int> dis(WIDTH, HEIGHT);
+// 시드값을 얻기 위한 random_device 생성.
+std::random_device rd;
+
+// random_device 를 통해 난수 생성 엔진을 초기화 한다.
+std::mt19937 gen(rd());
+
+// 0 부터 3까지 균등하게 나타나는 난수열을 생성하기 위해 균등 분포 정의.
+std::uniform_int_distribution<int> dis(400, 600); // 앞이 min값을 넣어줘야 하며, 뒤 max값을 넣어줘야 한다.
 
 Walker::Walker() 
 {
